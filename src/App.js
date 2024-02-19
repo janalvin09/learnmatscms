c528484ba150fcf8bb561fdfd74dafd9ee37ebcd
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-02-19 11:33:02 +0800

    changed smth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 
-const gian = 1;
-
-function App() {
+const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen text-green-700 hover:animate-spin">
       Welcome to learning material CMS
     </div>
   );
-}
+};
 
 export default App;
