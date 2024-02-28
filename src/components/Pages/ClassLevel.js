0af0a2baa760b20bd743f460615f3c8aec8cc3fc
Argie <argie.b@flowerstore.ph>
2024-02-29 02:57:17 +0800

    initial authentication

--- src/components/Pages/ClassLevel.js
+++ src/components/Pages/ClassLevel.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { CreateButton } from "../Partial/CreateButton";
 
 export const ClassLevel = () => {
   // Sample data for grades
-  const grades = ["Grade 1", "Grade 2", "Grade 3"]; // Add more grades as needed
-
+  const grades = ["Grade 1", "Grade 2", "Grade 3", "Grade 4"]; // Add more grades as needed
 
   return (
     <div className="p-8 classlevel_main">
-      <h1 className="mb-8 text-4xl font-bold">Class Levels</h1>
+      <h1 className="mb-8 text-4xl font-bold">List of Class Levels</h1>
 
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <CreateButton
+        destination={"/dashboard/classlevel/create"} 
+        styles={"create-btn-main bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5"}
+        label={"add classlevel"}
+      />
+
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {grades.map((grade, index) => (
           <div
             key={index}
@@ -20,9 +26,7 @@ export const ClassLevel = () => {
             </div>
             <div className="p-6">
               <p className="text-gray-600">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                auctor justo vel justo eleifend, sed pellentesque elit
-                consectetur.
+                {grade} Reading Materials📖
               </p>
               {/* Add more content or actions specific to each grade */}
             </div>
--- src/components/Pages/Result.js
+++ src/components/Pages/Result.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { CreateButton } from "../Partial/CreateButton";
 
 export const Result = () => {
   return (
-    <div className='result_main w-full flex justify-center items-center'>
-      Hello from result screen
+    <div className='result_main w-full p-8'>
+      <h1 className="mb-8 text-4xl font-bold">List of Results</h1>
+        <CreateButton
+          destination={"/dashboard/result/create"} 
+          styles={"create-btn-main bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5"}
+          label={"add result"}
+        />
     </div>
     
   )
